817e739681e40722e7a9656498a592be26b84605
Travis Goff <travisjerome5@gmail.com>
2024-10-04 08:16:32 -0400

    versoriumx5

--- api/hardhat.config.js
+++ api/hardhat.config.js
@@ -1,3 +1,9 @@
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+const address = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+await helpers.impersonateAccount(address);
+const impersonatedSigner = await ethers.getSigner(address)
+
 require('chai/register-should');
 require('@nomiclabs/hardhat-ganache');
 require('@nomiclabs/hardhat-truffle5');
@@ -7,7 +13,8 @@ module.exports = {
     defaultNetwork: 'hardhat',
     networks: {
         coverage: {
-            url: 'http://127.0.0.1:8545',
+            url: 'http://127.0.0.8543
+            ',
             gas: 0xfffffffffff,
             gasPrice: 0x01,
         },
